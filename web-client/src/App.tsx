3bf0f38cf1f5c5a60cb1b97b4820e0e894b44d98
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-04-28 18:36:01 +0200

    Login

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Login from './components/Login'
 
 import React from 'react'
 
@@ -8,17 +9,18 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+        </a>
+                <Login/>
             </header>
         </div>
     )
