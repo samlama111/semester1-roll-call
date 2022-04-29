182f6e3b6aae283e28277eecc7f7e18da9682a77
mstrucka <mstrucka@gmail.com>
2022-04-29 09:29:17 +0200

    Header, Footer created

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,27 +1,21 @@
 import './App.css'
-import Login from './components/Login'
 
 import React from 'react'
 
-import logo from './logo.svg'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Login from './components/Login'
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-        </a>
+                <Header/>
                 <Login/>
+                <p />
+                <Footer />
             </header>
+
         </div>
     )
 }
