d905121bc5c38bb958ef9fd0a254222e53a03375
Sam <sam.horacek@gmail.com>
2022-04-30 20:15:02 +0200

    Resolve of merge conflict

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -10,21 +10,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-<<<<<<< HEAD
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-=======
                 <Header/>
->>>>>>> 182f6e3b6aae283e28277eecc7f7e18da9682a77
                 <Login/>
                 <p />
                 <Footer />
