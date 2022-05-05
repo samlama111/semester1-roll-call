7ad0ab99de41cf1adf44d86df5475b2e33219fe9
Sam <sam.horacek@gmail.com>
2022-05-05 11:37:26 +0200

    Set-up of MUI theme

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,15 +1,28 @@
 import './styles/index.css'
 
+import { createTheme, ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Router from './content/Router'
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2',
+            contrastText: '#fff' // button text white instead of black
+        }
+    }
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
+
 root.render(
     <React.StrictMode>
-        <Router />
+        <ThemeProvider theme={theme}>
+            <Router />
+        </ThemeProvider>
     </React.StrictMode>
 )
