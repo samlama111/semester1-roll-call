d94bd8898f0450349d22fd769824b1b42ef8f2b1
samlama111 <sam.horacek@gmail.com>
2022-05-14 09:36:12 +0200

    Update of user-related types

--- backend/src/shared/db/DbUser.ts
+++ backend/src/shared/db/DbUser.ts
@@ -1,6 +1,7 @@
 import { DbBaseEntity } from "./DbBaseEntity";
 
 export interface DbUser extends DbBaseEntity {
+    uid: string;
     firstname: string;
     lastname: string;
     password: string;
