67ea774ca1e9d0c13ce5e655db7ae6736e684b84
samlama111 <sam.horacek@gmail.com>
2022-05-14 15:19:38 +0200

    Fix of auth error on get attendance

--- web-client/src/services/attendanceService.ts
+++ web-client/src/services/attendanceService.ts
@@ -1,8 +1,10 @@
-import { client } from '../client'
+import { client, getTeacherId } from '../client'
 
 export async function getAttendanceByCourse(courseId: string) {
+    const token = await getTeacherId()
     const res = await client.callApi('attendance/GetByCourse', {
-        course_id: courseId
+        course_id: courseId,
+        jwtToken: token
     })
     return res
 }
