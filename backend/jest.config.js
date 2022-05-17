28c19483bed74fbfdbf3d2b5fcd198667ec72eb7
samlama111 <sam.horacek@gmail.com>
2022-05-17 15:44:49 +0200

    Set-up of Mongo database tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const tsPreset = require('ts-jest/jest-preset')
+const mongoPreset = require('@shelf/jest-mongodb/jest-preset')
+
 module.exports = {
-    preset: 'ts-jest',
+    ...tsPreset, 
+    ...mongoPreset,
     testEnvironment: 'node',
     testTimeout: 15000
 }
