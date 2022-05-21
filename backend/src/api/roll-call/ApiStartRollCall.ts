29ff413e3e482b517c852f55bfff7c42335fc970
samlama111 <sam.horacek@gmail.com>
2022-05-21 16:51:51 +0200

    Refactor of start roll call function

--- backend/src/api/roll-call/ApiStartRollCall.ts
+++ backend/src/api/roll-call/ApiStartRollCall.ts
@@ -1,37 +1,16 @@
-import { ObjectId } from 'mongodb'
 import { ApiCall } from 'tsrpc'
 
-import { addEnrollmentToCourse, getMostRecentTeachersCourseEnrollment } from '../../db/Course'
-import { validateObjectId } from '../../helpers/validator'
-import { DbEnrollment } from '../../shared/db/DbEnrollment'
+import { startRollCall } from '../../models/StartRollCall'
 import { ReqStartRollCall, ResStartRollCall } from '../../shared/protocols/roll-call/PtlStartRollCall'
 
 export async function ApiStartRollCall(call: ApiCall<ReqStartRollCall, ResStartRollCall>) {
-    if (!validateObjectId(call.req.course_id)) {
-        call.error('Use a valid course id')
-        return 
-    }
-    const isCourseEnrollmentActive = await getMostRecentTeachersCourseEnrollment(call.currentUserId, call.req.course_id)
+    const startedRollCall = await startRollCall(call.req.course_id, call.currentUserId)
 
-    if (isCourseEnrollmentActive) {
-        call.error('Enrollment already active')
+    if (!startedRollCall.value) { 
+        if (startedRollCall.errorMessage) call.error(startedRollCall.errorMessage)
         return
     }
-    const newEnrollment: DbEnrollment = {
-        _id: new ObjectId(),
-        date: new Date().toISOString(),
-        roll_call_started: true,
-        enrolled_student_ids: [],
-    }
-
-    const res = await addEnrollmentToCourse(call.req.course_id, newEnrollment)
-
-    if (!res.acknowledged && res.modifiedCount < 1) {
-        call.error('Role call could not be started')
-        return
-    }
-
     call.succ({
-        roll_call: newEnrollment
+        roll_call: startedRollCall.value
     })
 }
---
+++ backend/src/models/StartRollCall.ts
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb'
+
+import { addEnrollmentToCourse, getMostRecentTeachersCourseEnrollment } from '../db/Course'
+import { validateObjectId } from '../helpers/validator'
+import { DbEnrollment } from '../shared/db/DbEnrollment'
+import { ModelReturnType } from './ModelReturnType'
+    
+export const startRollCall = async (courseId: ObjectId, teacherId: string | undefined):
+ Promise<ModelReturnType<DbEnrollment | undefined>> => {
+    if (!validateObjectId(courseId)) {
+        return {
+            value: undefined,
+            errorMessage: 'Use a valid course id'
+        }
+    }
+    const isCourseEnrollmentActive = await getMostRecentTeachersCourseEnrollment(teacherId, courseId)
+
+    if (isCourseEnrollmentActive) {
+        return {
+            value: undefined,
+            errorMessage: 'Enrollment already active'
+        }
+    }
+    const newEnrollment: DbEnrollment = {
+        _id: new ObjectId(),
+        date: new Date().toISOString(),
+        roll_call_started: true,
+        enrolled_student_ids: [],
+    }
+
+    const res = await addEnrollmentToCourse(courseId, newEnrollment)
+
+    if (!res.acknowledged && res.modifiedCount < 1) {
+        return {
+            value: undefined,
+            errorMessage: 'Role call could not be started'
+        }
+    }
+
+    return {
+        value: newEnrollment
+    }
+
+}
