8344f57a3597b2f4f965a3b0c95eef0ba56642a2
samlama111 <sam.horacek@gmail.com>
2022-05-23 09:13:30 +0200

    Added list teacher functionality

--- backend/src/db/Teacher.ts
+++ backend/src/db/Teacher.ts
@@ -6,3 +6,8 @@ const collectionName = 'Teacher'
 export const insertTeacher = async (newTeacher: DbTeacher) => {
     return Global.collection(collectionName).insertOne(newTeacher)
 }
+
+export const getAllTeachers = async () => {
+    return Global.collection(collectionName).find({
+    }).toArray()
+}
