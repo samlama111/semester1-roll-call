58881f595d32ef1c18033ca0be49a028589b461b
samlama111 <sam.horacek@gmail.com>
2022-06-03 13:58:50 +0200

    Update of location tests

--- backend/test/unit/helper/location.test.ts
+++ backend/test/unit/helper/location.test.ts
@@ -1,12 +1,18 @@
 import { getDistanceFromLatLonInKm } from '../../../src/helpers/location'
 
+const validLatitude = 10
+const validLongitude = 15
+const invalidLatitude = 91
+const invalidLongitude = 181
+
 describe('Location', () => {
     beforeEach(() => {
         jest.clearAllMocks()
     })
 
     it.each([
-        [10, 15, 20, 25, 1544.755]
+        [validLatitude, validLongitude, 20, 25, 1544.755],
+        [validLatitude, validLongitude, validLatitude, validLongitude, 0],
     ])('should return a valid distance between coordinates', async (
         latitude1, 
         longitude1, 
@@ -14,21 +20,21 @@ describe('Location', () => {
         longitude2,
         distance
     ) => {
-          
         const expectedLocation = getDistanceFromLatLonInKm(latitude1, longitude1, latitude2, longitude2)
         expect(expectedLocation).toBeCloseTo(distance)
     })
 
     it.each([
-        // invalid latitude
-        [91, 15, 20, 25]
+        [invalidLatitude, validLongitude, 20, 25],
+        [validLatitude, invalidLongitude, 20, 25],
+        [validLatitude, invalidLongitude, 20, invalidLongitude],
+        [validLatitude, invalidLongitude, invalidLatitude, invalidLongitude],
     ])('should return an undefined distance between coordinates', async (
         latitude1, 
         longitude1, 
         latitude2, 
         longitude2
     ) => {
-          
         const expectedLocation = getDistanceFromLatLonInKm(latitude1, longitude1, latitude2, longitude2)
         expect(expectedLocation).toEqual(undefined)
     })
